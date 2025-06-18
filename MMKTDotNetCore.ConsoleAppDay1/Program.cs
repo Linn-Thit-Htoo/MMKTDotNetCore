f5598765087965685f546f4df01315948ad1c6ea
Lin Thit Htoo <lth1212001@gmail.com>
2025-06-18 21:10:05 +0630

    Add project files.

---
+++ MMKTDotNetCore.ConsoleAppDay1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+
+public class Program
+{
+    public static void Main(string[] args) // entry point
+    {
+        int a = 10;
+        decimal b = 10.0M;
+        float c = 10.0F;
+        long phoneNumber = 09773871112;
+        string firstName = "Lin Thit";
+        string lastName = "Htoo";
+
+        // lastName = "overwritten"; // reassigning value
+
+        int result = 1 + 1; // int
+
+        //StringBuilder stringBuilder = new();
+        //stringBuilder.Append("Lin Thit");
+        //stringBuilder.Append(" Htoo");
+        //Console.WriteLine(stringBuilder.ToString());
+
+        // concatenate
+        string fullName = firstName + " " + lastName;
+        Console.WriteLine(fullName);
+
+        // Your Full Name is:
+        // interpolation
+        Console.WriteLine($"Your Full Name is: {fullName}");
+        Console.WriteLine("Your Full Name is: {0}", fullName);
+    }
+
+    public static async Task RunAsync() // void
+    {
+        await Task.Delay(100);
+    }
+}
