2485c3dde96a0c7bf0e13a1680c82694fea40945
Lin Thit Htoo <lth1212001@gmail.com>
2025-06-21 20:43:13 +0630

    feat: added sample web app

---
+++ MMKTDotNetCore.SampleWebApp/Controllers/TestController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MMKTDotNetCore.SampleWebApp.Controllers
+{
+    public class TestController : Controller
+    {
+        [ActionName("TestIndex")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
